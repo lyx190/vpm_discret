66157dd39e5c6b2e01809f95ff75fd253fc72e00
lyx190 <linyuxiang190@gmail.com>
2019-12-21 15:19:50 +0000

    test 2

--- train_VPM.sh
+++ train_VPM.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PCB_tri_path='logs'
+PCB_tri_path='~/logs'
 
 python3 -W ignore PCB_tri_partial_column.py -d market -a resnet50_pseudo_column -b 64 -j 1 --epochs 60 --log ${PCB_tri_path} --combine-trainval --feature 256 --height 384 --width 128 --step-size 40 --data-dir ~/Market/  --step-size 80  --lr 0.1 --num_parts 6  --epochs 130  #--evaluate
 
