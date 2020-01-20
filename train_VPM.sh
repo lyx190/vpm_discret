6b243a9b6826f6826c5cc2257511b086b20f13fb
lyx190 <linyuxiang190@gmail.com>
2020-01-20 12:34:55 +0000

    修改 train_VPM.sh

--- train_VPM.sh
+++ train_VPM.sh
@@ -2,7 +2,7 @@
 
 PCB_tri_path='~/logs'
 
-python3 -W ignore PCB_tri_partial_column.py -d market -a resnet50_pseudo_column -b 64 -j 1 --epochs 60 --log ${PCB_tri_path} --combine-trainval --feature 256 --height 384 --width 128 --step-size 40 --data-dir ~/Market/  --step-size 80  --lr 0.1 --num_parts 6  --epochs 130  #--evaluate
+python3 -W ignore PCB_tri_partial_column.py -d market -a resnet50_pseudo_column -b 64 -j 1 --epochs 60 --log ${PCB_tri_path} --combine-trainval --feature 256 --height 384 --width 128 --step-size 40 --data-dir /mnt/datasets/Market/  --step-size 80  --lr 0.1 --num_parts 6  --epochs 130  #--evaluate
 
 cp reid/utils/data/transforms.py ${PCB_tri_path}/
 cp reid/trainers_tri_pseudo_column.py ${PCB_tri_path}/
